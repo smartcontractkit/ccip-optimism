4aaced924dd37dc76c1293c4cd51268977820db1
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-12-16 16:23:46 -0800

    Implement function to fetch optimism proof

---
+++ src/abis/index.ts
@@ -0,0 +1,7 @@
+import addressManagerABI from "./addressManager.json"
+import stateCommitmentChain from "./stateCommitmentChain.json"
+
+export {
+    addressManagerABI,
+    stateCommitmentChain
+}
